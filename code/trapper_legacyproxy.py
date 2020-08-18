15b05bbfb94547002abc311598a4e5e8f59236f0
Ivan Belokobylskiy <belokobylskij@gmail.com>
2020-08-18 14:13:35 +0300

    Downgrade zabbix server response

--- code/trapper_legacyproxy.py
+++ code/trapper_legacyproxy.py
@@ -1,15 +1,25 @@
 import asyncio
 import json
 import logging
+import os
+import re
 import struct
 
 
 logger = logging.getLogger(__name__)
+os.environ.setdefault('ZBX_SERVER', '127.0.0.1')
+os.environ.setdefault('ZBX_PORT', '10051')
 
 PROXY_ADDRESS = '0.0.0.0'
-PROXY_PORT = 10060
-ZABBIX_ADDRESS = '127.0.0.1'
-ZABBIX_PORT = 10050
+PROXY_PORT = 10051
+
+ZABBIX_SERVER = os.environ['ZBX_SERVER']
+ZABBIX_PORT = int(os.environ['ZBX_PORT'])
+
+info_rx = re.compile(
+    r"processed: (\d+); failed: (\d+); total: (\d+); seconds spent: ([\d.]+)]"
+)
+info_replacement = "Processed {} Failed {} Total {} Seconds spent {}}"
 
 
 def packed2data(packed_data: bytes) -> bytes:
@@ -42,6 +52,31 @@ class ZabbixLegacyClientProxy:
             return json.dumps(content).encode('utf-8')
         return data
 
+    @staticmethod
+    def upgrade_response(data: bytes):
+        """
+        Fix response['info']
+        """
+
+        upgraded = False
+        try:
+            content = json.loads(data)
+        except ValueError:
+            return data
+        if isinstance(content, dict):
+            r = info_rx.match(content.get('info'))
+            if r:
+                content['info'] = info_replacement.format(
+                    r.group(1),
+                    r.group(2),
+                    r.group(3),
+                    r.group(4),
+                )
+                upgraded = True
+        if upgraded:
+            return json.dumps(content).encode('utf-8')
+        return data
+
     @classmethod
     async def request_replacer(cls, reader, writer):
         buffer = b''
@@ -59,6 +94,21 @@ class ZabbixLegacyClientProxy:
             writer.close()
 
     @classmethod
+    async def response_replacer(cls, reader, writer):
+        buffer = b''
+        try:
+            while not reader.at_eof():
+                buffer += await reader.read(2048)
+            try:
+                data = cls.upgrade_response(buffer)
+            except (IndexError, ValueError):
+                logger.exception("Cannot upgrade response {}".format(buffer))
+                data = buffer
+            writer.write(data)
+        finally:
+            writer.close()
+
+    @classmethod
     async def transparent_pipe(cls, reader, writer):
         try:
             while not reader.at_eof():
@@ -69,11 +119,11 @@ class ZabbixLegacyClientProxy:
     async def handle_client(self, local_reader, local_writer):
         try:
             remote_reader, remote_writer = await asyncio.open_connection(
-                ZABBIX_ADDRESS,
+                ZABBIX_SERVER,
                 ZABBIX_PORT,
             )
             request_pipe = self.request_replacer(local_reader, remote_writer)
-            response_pipe = self.transparent_pipe(remote_reader, local_writer)
+            response_pipe = self.response_replacer(remote_reader, local_writer)
             await asyncio.gather(request_pipe, response_pipe)
         finally:
             local_writer.close()
